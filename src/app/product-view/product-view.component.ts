2ffcc72ff81b88672ac999d6e1ee2c7c02f65933
agam <agamgupta1988@gmail.com>
2019-06-16 13:08:39 -0500

    added input in component and tested in app components

--- src/app/product-view/product-view.component.ts
+++ src/app/product-view/product-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-view',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-view.component.css']
 })
 export class ProductViewComponent implements OnInit {
+
+@Input()
+productName: string;
+
 
   constructor() { }
 
