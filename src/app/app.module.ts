63d6557f5737e1cc60ae615a57fbc705606c86fd
agam <agamgupta1988@gmail.com>
2019-06-16 13:00:03 -0500

    added web components and created new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductViewComponent } from './product-view/product-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductViewComponent
   ],
   imports: [
     BrowserModule
